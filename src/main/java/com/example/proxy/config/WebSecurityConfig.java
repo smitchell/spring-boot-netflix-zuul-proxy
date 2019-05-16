65b68abf87cae9b4ac7992b69dbafd1879495f80
Steve Mitchell <steve.mitchell@mobilecapitalgroup.com>
2019-05-16 15:44:39 -0500

    Added props from https://www.baeldung.com/spring-security-oauth2-enable-resource-server-vs-enable-oauth2-sso

--- src/main/java/com/example/proxy/config/WebSecurityConfig.java
+++ src/main/java/com/example/proxy/config/WebSecurityConfig.java
@@ -1,9 +1,6 @@
 package com.example.proxy.config;
 
-import java.util.Arrays;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
