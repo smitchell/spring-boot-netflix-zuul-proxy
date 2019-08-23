de35944bf97bd60ba44012217b0b50c584d41db0
Steve Mitchell <steve.mitchell@mobilecapitalgroup.com>
2019-08-23 12:59:11 -0500

    Changed to run locally

--- src/main/java/com/example/proxy/Application.java
+++ src/main/java/com/example/proxy/Application.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy
-@EnableDiscoveryClient
 @SpringBootApplication
 public class Application {
 
